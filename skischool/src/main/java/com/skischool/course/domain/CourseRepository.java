51c9876f07c5e77d655efa015685d0fa4a15086a
Cappl1 <119538490+Cappl1@users.noreply.github.com>
2023-06-14 15:25:19 +0200

    Delete skischool directory

--- skischool/src/main/java/com/skischool/course/domain/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skischool.course.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface CourseRepository extends CrudRepository<Course, Long> {
-
-    // Custom queries if needed...
-}
